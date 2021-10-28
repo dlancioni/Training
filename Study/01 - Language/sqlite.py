b75bdb37ad65199134df34b8dc69686a59316b2a
David Lancioni <dlancioni@gmail.com>
2021-10-28 15:07:48 -0300

    Add sqlite demo

--- Study/01 - Language/sqlite.py
+++ Study/01 - Language/sqlite.py
@@ -12,31 +12,35 @@ def create_connection(db_file):
 
         # Get Connection
         conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
+        conn.isolation_level = None
 
         # Create table
-        sql = """ create table if not exists tb_1 (id integer, name text) """
         cursor = conn.cursor()
-        cursor.execute(sql)
+        cursor.execute("create table if not exists tb_1 (id integer, name text)")
 
         # Insert data
-        sql = """ insert into tb_1 (id, name) values (1, 'David') """
         cursor = conn.cursor()
-        cursor.execute(sql)
-        print(cursor.lastrowid)
+        cursor.execute("begin")
+        cursor.execute("insert into tb_1 (id, name) values (1, 'David')")
+        cursor.execute("insert into tb_1 (id, name) values (2, 'Renata')")
+        cursor.execute("commit")
+        print(cursor.lastrowid) # Print 2 (note it is not identity)
 
         # Select data
         cursor = conn.cursor()
-        cursor.execute("SELECT id, name from tb_1 where id = ?", (1,))
+        cursor.execute("SELECT id, name from tb_1 where id = ?", (2,))
         rows = cursor.fetchall()
+
+        # Field names
+        print(cursor.description[0][0]) # id
+        print(cursor.description[0][1]) # name        
+
+        # Field values
         for row in rows:
             print(str(row[0]) + " " + row[1])
-        
-        # Cursor info
-        print(cursor.description)
-
 
     except Error as e:
+        cursor.execute("rollback")
         print(e)
     finally:
         if conn:
