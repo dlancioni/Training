172a90316e616d87a5e8f9875dbf5620be76e18e
David Lancioni <dlancioni@gmail.com>
2021-11-02 16:43:26 -0300

    Calendar and datetime

---
+++ Study/01 - Language/datetime/calendar.py
@@ -0,0 +1,29 @@
+from datetime import date
+import calendar
+import os
+os.system("cls")
+
+## print calendar for given year/month
+print(calendar.month(2021, 11))
+
+# day_name is an array
+print (calendar.day_name[0])
+print (calendar.day_name[1])
+print (calendar.day_name[2])
+print (calendar.day_name[3])
+print (calendar.day_name[4])
+print (calendar.day_name[5])
+print (calendar.day_name[6])
+
+# print day of week of given date
+def dayOfWeek(dt):
+    # expected string format: MM DD YYYY
+    yy = int(dt[6:10])
+    mm = int(dt[0:2])
+    dd = int(dt[3:5])
+    # create date
+    dt = date(yy, mm, dd)
+    return calendar.day_name[dt.weekday()].upper()
+
+os.system("cls")
+print(dayOfWeek("11 02 2021"))
--- Study/01 - Language/datetime/datetime.py
+++ Study/01 - Language/datetime/datetime.py
@@ -1,10 +1,14 @@
-import time;
-import calendar;
+# https://docs.python.org/3/library/datetime.html
 from datetime import datetime
 from datetime import timedelta 
+from datetime import date
 
+# calculate date
 x = datetime.now() + timedelta(days=-1)
 
+# format date output
 x = x.strftime("%Y-%m-%d")
-
 print(x)
+
+# create date from integer
+print(date(2002, 12, 4).weekday())
