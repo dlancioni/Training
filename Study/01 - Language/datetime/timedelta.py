6c8d99ec8a5c25dfb77243be6a2bfe996afdd584
David Lancioni <dlancioni@gmail.com>
2021-11-09 16:16:02 -0300

    Playing with dates

--- Study/01 - Language/datetime/timedelta.py
+++ Study/01 - Language/datetime/timedelta.py
@@ -2,29 +2,30 @@ import os
 os.system("cls")
 
 # https://miguendes.me/how-to-use-datetimetimedelta-in-python-with-examples
-# pip install python-dateutil
 
 from datetime import datetime
 from datetime import timedelta
-from dateutil.relativedelta import relativedelta
+from dateutil.relativedelta import relativedelta  # pip install python-dateutil
 
+# define date to work
+dt = datetime(2021, 12, 1, 0, 0)
 
 # Add/remove days
-print(datetime(2021, 12, 1) + timedelta(days =+ 1))
-print(datetime(2021, 12, 1) + timedelta(days =-1 ))
+print(dt + timedelta(days =+ 1))
+print(dt + timedelta(days =-1 ))
 
 # Add/remove minutes
-print(datetime(2021, 12, 1, 0, 0) + timedelta(minutes =+ 5))
-print(datetime(2021, 12, 1, 0, 0) + timedelta(minutes =- 5))
+print(dt + timedelta(minutes =+ 5))
+print(dt + timedelta(minutes =- 5))
 
 # Add/remove weeks
-print(datetime(2021, 12, 1, 0, 0) + timedelta(weeks =+ 5))
-print(datetime(2021, 12, 1, 0, 0) + timedelta(weeks =- 5))
+print(dt + timedelta(weeks =+ 5))
+print(dt + timedelta(weeks =- 5))
 
 # Add/remove months
-print(datetime(2021, 12, 1, 0, 0) + relativedelta(months =+ 5))
-print(datetime(2021, 12, 1, 0, 0) + relativedelta(months =- 5))
+print(dt + relativedelta(months =+ 5))
+print(dt + relativedelta(months =- 5))
 
 # Add/remove years
-print(datetime(2021, 12, 1, 0, 0) + relativedelta(years =+ 5))
-print(datetime(2021, 12, 1, 0, 0) + relativedelta(years =- 5))
+print(dt + relativedelta(years =+ 5))
+print(dt + relativedelta(years =- 5))
