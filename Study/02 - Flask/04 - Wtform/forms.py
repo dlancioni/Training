73c256018397b39c3d6b798c5bd7de2535c260aa
David Lancioni <dlancioni@gmail.com>
2021-12-16 17:10:46 -0300

    Organize forms

--- Study/02 - Flask/04 - Wtform/forms.py
+++ Study/02 - Flask/04 - Wtform/forms.py
@@ -16,6 +16,8 @@ class Form1(FlaskForm):
     firstname = StringField(label=("First Name:"), validators=[InputRequired(message="First name is mandatory")])
     lastname = StringField(label=("Last Name:"), validators=[])
     email = EmailField(label=("Email"), validators=[Email(message="Email field looks incorrent")])
+    country = SelectField("Country:", choices=[("","Select"), ("AR","Argentina"), ("BR","Brazil")])
+    state = SelectField("State:", choices=[("","Select"), ("SP","Sao Paulo"), ("BA","Buenos Aires")])    
     password = StringField(label=("Password:"), validators=[length(min=2, max=5, message="Password must be %d and %d characteres long")])    
     submit = SubmitField(label=("Submit"))
     
@@ -23,13 +25,3 @@ class Form1(FlaskForm):
     def validate_lastname(self, field):
         if len(field.data) == 0:
             raise ValidationError("Last name is mandatory")
-
-class Form2(FlaskForm):
-    username = StringField(label=("Username:"), validators=[])
-    password = PasswordField(label=("Password:"), validators=[length(min=2, max=5, message="Password must be %d and %d characteres long")])    
-    submit = SubmitField(label=("Submit"))
-
-class Form3(FlaskForm):
-    country = SelectField("Country:", choices=[("AR","Argentina"), ("BR","Brazil")])
-    state = SelectField("State:", choices=[("SP","Sao Paulo"), ("BA","Buenos Aires")])
-    submit = SubmitField(label=("Submit"))
--- Study/02 - Flask/04 - Wtform/module1.py
+++ Study/02 - Flask/04 - Wtform/module1.py
@@ -1,27 +1,12 @@
 from flask import Blueprint, render_template, session, request
-from forms import Form1, Form2, Form3
+from forms import Form1
 
 module1 = Blueprint('module1',__name__)
 
 @module1.route('/', methods=('GET', 'POST'))
-def index():
-    return "Please inform the form (form1, form2) in the URL"
-
 @module1.route('/form1', methods=('GET', 'POST'))
 def form1():
     form = Form1()
     if form.validate_on_submit():
         return f"<h1> Welcome {form.firstname.data} {form.lastname.data} </h1>"
     return render_template('form1.html', form=form)
-
-@module1.route('/form2', methods=('GET', 'POST'))
-def form2():
-    form = Form2()
-    if form.validate_on_submit():
-        return f"<h1> Authenticated {form.username.data} </h1>"
-    return render_template('form2.html', form=form)
-
-@module1.route('/form3', methods=('GET', 'POST'))
-def form3():
-    form = Form3()
-    return render_template('form3.html', form=form)
