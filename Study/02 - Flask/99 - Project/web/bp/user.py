0459dcd1fc35e62db7343758186d12a28bf789de
David Lancioni <dlancioni@gmail.com>
2021-12-28 16:01:02 -0300

    Add templates

--- Study/02 - Flask/99 - Project/web/bp/user.py
+++ Study/02 - Flask/99 - Project/web/bp/user.py
@@ -1,14 +1,16 @@
+from flask import Flask, render_template
 from flask.blueprints import Blueprint
 from src.models.user import User
 
-bp_user = Blueprint('bp_user', __name__, template_folder='templates', static_folder='static')
+bp_user = Blueprint('bp_user', __name__, template_folder = "../templates", static_folder="static")
 
 @bp_user.route('/user_list')
 def user_list():
-  try:
-      users = User.query.all()
-      users = [user.username for user in users]
-  except Exception:
-      users = "Something went wrong"
+
+    try:
+        users = User.query.all()
+        users = [user.username for user in users]
+    except Exception:
+        users = "Something went wrong"
       
-  return "Userlist: %s " % users
+    return render_template('user.html', users=users)
