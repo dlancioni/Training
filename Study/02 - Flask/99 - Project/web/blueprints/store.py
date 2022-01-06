3701720cc969c93001cddc8dad7a0a2fe1f2ac6e
David Lancioni <dlancioni@gmail.com>
2022-01-06 15:07:20 -0300

    Create screen

--- Study/02 - Flask/99 - Project/web/blueprints/store.py
+++ Study/02 - Flask/99 - Project/web/blueprints/store.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 from flask.blueprints import Blueprint
+from src.models.category import Category
 
 store = Blueprint("store", __name__, template_folder = "../templates", static_folder="static")
 
 @store.route("/")
-def home():   
-    return render_template("store.html")
+def home():
+    category = Category.query.filter().order_by("name")
+    return render_template("store.html", category=category)
