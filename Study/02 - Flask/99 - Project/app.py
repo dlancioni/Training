21e962737e9568ec0f4155512a47ad30770f1bfa
David Lancioni <dlancioni@gmail.com>
2022-01-07 14:56:58 -0300

    store to home

--- Study/02 - Flask/99 - Project/app.py
+++ Study/02 - Flask/99 - Project/app.py
@@ -4,7 +4,7 @@ from src.db.migrations import create_db
 
 from web.blueprints.user import user
 from web.blueprints.category import category
-from web.blueprints.store import store
+from web.blueprints.home import home
 
 def setup_database(app):
     app.config['DEBUG'] = True
@@ -12,7 +12,7 @@ def setup_database(app):
     db.init_app(app)
     
 def setup_blueprints(app):
-    app.register_blueprint(store, url_prefix = "/")                       # home page (no prefix ever)
+    app.register_blueprint(home, url_prefix = "/")                       # home page (no prefix ever)
     app.register_blueprint(user, url_prefix = "/admin/user")              # users maintenance
     app.register_blueprint(category, url_prefix = "/admin/category")      # category maintenance
 
--- Study/02 - Flask/99 - Project/web/blueprints/home.py
+++ Study/02 - Flask/99 - Project/web/blueprints/home.py
@@ -6,10 +6,10 @@ from src.core.product import Product
 from src.core.category import Category
 
 
-store = Blueprint("store", __name__, template_folder = "../templates", static_folder="static")
+home = Blueprint("home", __name__, template_folder = "../templates", static_folder="static")
 
-@store.route("/")
-def home():
+@home.route("/")
+def main():
     
     product = Product(db)
     category = Category(db)
@@ -17,4 +17,4 @@ def home():
     rs1 = category.get_category()
     rs2 = product.get_product()
     
-    return render_template("store.html", category=rs1, product=rs2)
+    return render_template("home.html", category=rs1, product=rs2)
