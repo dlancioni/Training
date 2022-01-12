8d7ee1a2ea465ea5ce2e7fb4212e29e1b7d03f2b
David Lancioni <dlancioni@gmail.com>
2022-01-12 11:56:44 -0300

    Test alquemy

--- Study/02 - Flask/03 - Sqlalchemy/blogapp/routes.py
+++ Study/02 - Flask/03 - Sqlalchemy/blogapp/routes.py
@@ -1,8 +1,20 @@
 from blogapp import app
-from blogapp.models import User, Post
+from blogapp.models import db, User, Post
 from flask import render_template, url_for, flash, redirect
 
 @app.route("/")
 def home():
     user = User.query.get(1)
     return f"bem vindo ao blogapp {user.username}"
+
+@app.route("/query1")
+def query1():
+    user = db.session.query(User).filter(User.id == 2).all()
+    return f"bem vindo ao blogapp {(user)}"
+
+@app.route("/query2")
+def query2():
+    where = User.id == 1
+    where += User.id == 2
+    user = db.session.query(User).filter(where).all()
+    return f"bem vindo ao blogapp {(user)}"
