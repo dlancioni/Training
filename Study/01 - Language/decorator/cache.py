bf4b8399502ef64a015b4affbd870a4207fd0161
David Lancioni <dlancioni@gmail.com>
2022-03-09 21:07:24 -0300

    Delegates/decorattors

--- Study/01 - Language/decorator/cache.py
+++ Study/01 - Language/decorator/cache.py
@@ -11,9 +11,3 @@ def fibonacci(n):
         return 1
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
-
-
-start = time.time()
-print(fibonacci(35))
-end = time.time()
-print(end - start)
---
+++ Study/01 - Language/decorator/custom.py
@@ -0,0 +1,36 @@
+import os
+
+from sqlalchemy import false
+os.system("cls")
+
+
+def log(fn):
+    
+    # delegate the function to wrapper
+    def wrapper(*args, **kargs):
+        
+        # do something locally        
+        print("before")        
+        
+        # execute the function passed as parameter
+        result = fn(*args, **kargs)
+        
+        # do something locally        
+        print("after")
+        
+        # logic you can add
+        if result == 3:
+            return result
+        else:
+            return 0
+
+    # must return wrapper
+    return wrapper
+
+@log
+def sum(a, b):
+    return a+b
+
+x = sum(1, 1)
+x = sum(2, 1)
+print('ënd')
