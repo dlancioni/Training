9321456ba136a81118577ad5ba72be0679baf294
David Lancioni <dlancioni@gmail.com>
2022-03-24 11:34:55 -0300

    Studying

--- Study/01 - Language/challenge/LeetCode/lexical.py
+++ Study/01 - Language/challenge/LeetCode/lexical.py
@@ -5,10 +5,22 @@ os.system("cls")
 
 def index_for(letter):
     return ord(letter) + 1 - 97 # base 1
-print(index_for("k"))
-
 def char_at(index):
     return chr(index-1 + 97) # base 1
-#print(char_at(1))
 
 
+def solve(n, k):
+   string = ""
+   while n > 0:
+      letter = min(26, k-n+1)
+      string += chr(letter + ord('a') - 1)
+      k -= letter
+      n -= 1
+   return string[::-1]
+
+n = 4
+k = 6 
+#print(solve(n, k))
+
+
+print(chr(31))
---
+++ Study/01 - Language/core/unicode.py
@@ -0,0 +1,39 @@
+import os
+os.system("cls")
+
+# Base asc table
+# https://python-reference.readthedocs.io/en/latest/docs/str/ASCII.html
+# lookuptables.com
+#
+# index to char
+#
+# numbers
+print(chr(97))
+print(chr(122))
+
+#
+# char to index
+#
+# numbers
+print("numbers")
+print(ord("0"))
+print(ord("9"))
+
+# letters cap
+print("letters")
+print(ord("a"))
+print(ord("z"))
+
+# letters cap
+print("LETTERS")
+print(ord("A"))
+print(ord("Z"))
+
+# letters cap
+print("Ponctuation")
+print(ord(","))
+print(ord("."))
+print(ord(":"))
+
+
+print("	&mho;")
