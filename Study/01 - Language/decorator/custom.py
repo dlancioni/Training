d750587352e60230e85ae541cf7305ece399ff93
David Lancioni <dlancioni@gmail.com>
2022-04-16 19:47:30 -0300

    Decorator with parameter

--- Study/01 - Language/decorator/custom.py
+++ Study/01 - Language/decorator/custom.py
@@ -1,29 +1,10 @@
 import os
-
-from sqlalchemy import false
 os.system("cls")
 
 def log(fn):
-    
-    # delegate the function to wrapper
     def wrapper(*args, **kargs):
-        
-        # do something locally        
-        print("before")        
-        
-        # execute the function passed as parameter
         result = fn(*args, **kargs)
-        
-        # do something locally        
-        print("after")
-        
-        # logic you can add
-        if result == 3:
-            return result
-        else:
-            return 0
-
-    # must return wrapper
+        print(result)
     return wrapper
 
 @log
@@ -32,4 +13,3 @@ def sum(a, b):
 
 x = sum(1, 1)
 x = sum(2, 1)
-print('ënd')
