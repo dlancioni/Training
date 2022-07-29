8275c31507ff5a4be932f66d066c545b7b664a9a
David Lancioni <dlancioni@gmail.com>
2022-07-29 11:31:45 -0300

    Click command line

--- 01 - Language/click/main.py
+++ 01 - Language/click/main.py
@@ -1,11 +1,24 @@
 import click
+import os
+os.system("cls")
 
-msg = "Your name please"
+msg1 = "Your name please"
+msg2 = "Limpar tela"
+msg3 = "Exibir relatorio sintetico"
+msg4 = "Exibir relatorio analitico"
+help = "Ask something"
 
 @click.command()
-@click.option('--name', prompt=f'{msg}')
-def main(name):
-    click.echo(f"Hello {name}!")
+@click.option('--recon', prompt=f"{msg1}", help=f"help", default="config.cfg")
+@click.option('--c',     prompt=f"{msg2}", help=f"help", default=1)
+@click.option('--rs',    prompt=f"{msg3}", help=f"help", default=0)
+@click.option('--ra',    prompt=f"{msg4}", help=f"help", default=12)
+
+def main(recon, c, rs, ra):
+    print("Recon Name: ", recon)
+    print("Limpar tela: ", recon)
+    print("relatorio sintetico: ", rs)
+    print("relatorio analitico: ", ra)
     
 if __name__ == '__main__':
     main()
