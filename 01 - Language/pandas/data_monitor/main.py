cb6d2d48b13c33bba83d141dfa80b9534e9f19dd
David Lancioni <dlancioni@gmail.com>
2022-08-31 14:28:12 -0300

    Fixing messages

--- 01 - Language/pandas/data_monitor/main.py
+++ 01 - Language/pandas/data_monitor/main.py
@@ -1,8 +1,10 @@
 # https://pandas.pydata.org/docs/user_guide/merging.html
 
+import warnings
 import os
 import numpy as np
 import pandas as pd
+warnings.simplefilter(action="ignore", category=FutureWarning)
 
 BASE_FOLDER = "PS RiskDB Data Monitor"
 BASE_PATH = "c:\\lancid\\data\\"
@@ -25,19 +27,16 @@ def trim_all_columns(df):
     trim_strings = lambda x: x.strip() if isinstance(x, str) else x
     return df.applymap(trim_strings)
 
-def get_timed(level, dt, tm):
-    
+def get_timed(level, dt, tm):   
     file_name = f"{BASE_FOLDER} {dt} {tm}.csv"
     path = f"{BASE_PATH}{BASE_FOLDER} {dt}\\{file_name}"
     df1 = pd.read_csv(path, ",")    
-    df1 = trim_all_columns(df1)
-    
+    df1 = trim_all_columns(df1)    
     col_bl = "BusinessLine"
     col_lv = f"Level {level}"   
     df1 = df1[[col_bl, col_lv]]
     df1 = df1[df1[col_lv] == STATUS]
-    df1 = df1.groupby([col_bl])[col_lv].count().reset_index()
-    
+    df1 = df1.groupby([col_bl])[col_lv].count().reset_index()    
     df2 = get_empty(level)
     df = df1.set_index(col_bl).combine_first(df2.set_index(col_bl)).reset_index()
     df.rename(columns={col_lv:tm}, inplace=True)    
@@ -56,9 +55,22 @@ def main(dt, tms):
 
 
 dt = "20220826"
-tms = ["0700", "0800", "0900"]
-result = main(dt,tms)
+tms = []
+
+path = f"{BASE_PATH}{BASE_FOLDER} {dt}"
+files = os.listdir(path)
 os.system("cls")
-print(result)
-result.to_csv(f"{BASE_PATH}{BASE_FOLDER} {dt}\\result.csv") 
+print("Processing date ", dt)
+if len(files) == 0:
+    print("No file at ", path)
+else:
+    print("Processing ", len(files), " files")
+    for file in files:
+        if (file[32:36] != ""):
+            tms.append(file[32:36])
+    result = main(dt,tms)
+    print(result)
+    print("Results at ", path + "\\result.csv")
+    result.to_csv(path + "\\result.csv")
+
 
